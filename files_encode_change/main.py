94866bb0353fe06dace6a3a274d5cbd9eb39af9d
ozg <ouzhigangGBA@gmail.com>
2014-06-25 23:55:09 +0800

    files_encode_change

--- files_encode_change/main.py
+++ files_encode_change/main.py
@@ -36,9 +36,12 @@ class OzgMainFrame(wx.Frame):
 		self.runButton = wx.Button(mainPanel, cfg.WXID_BTN_RUN, cfg.STRINGS_BTN_RUN, pos = (175, 120), size = (80, 25))
 		self.Bind(wx.EVT_BUTTON, self.BtnOnClick, self.runButton)
 		self.runButton.SetDefault()
-					
+	
 	def BtnOnClick(self, event):
-		
+		"""
+		按钮点击的事件
+		"""
+
 		if event.GetEventObject().GetId() == cfg.WXID_BTN_SELECTED_DIR:
 			filesSelectDialog = wx.DirDialog(None, cfg.STRINGS_SELECTED_DIR_TEXT, style = wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
 
@@ -64,34 +67,100 @@ class OzgMainFrame(wx.Frame):
 					alert.ShowModal()
 					return
 				else:
-					#filesFilter = self.filesFilterTextCtrl.GetValue().split(u";")
-					#print filesFilter
-										
+					#启动子线程来执行转换编码的工作
 					self.childThread = Thread(target = self.EncodeChangeThread, args = ())
 					self.childThread.start()
 
 	def EncodeChangeThread(self):
+		"""
+		子线程执行转换编码的逻辑
+		"""
+
 		wx.CallAfter(self.EnableUI, False)
 		self.EncodeChange(self.dirTextCtrl.GetValue())
 		wx.CallAfter(self.FinishChange)
 		
 	def EncodeChange(self, dirPath):
+		"""
+		历遍目录来执行转换编码，这个是递归方法
+		"""
+
 		files = os.listdir(dirPath)
 		for f in files:
 			filePath = os.path.join(dirPath, f)
 			if os.path.isdir(filePath):
+				#是文件夹的话则继续执行本方法
 				self.EncodeChange(filePath)
 			else:
-				print u"%s %s" % (filePath, cfg.STRINGS_RUNNING_MSG)
+				#是文件的话则执行转换编码的流程
+
+				#判断是否为需要处理的文件
+				isExec = False
+				filesFilter = self.filesFilterTextCtrl.GetValue().split(u";")
+				for i in filesFilter:
+					if i == filePath[filePath.rfind("."):]:
+						isExec = True
+						break
+				
+				#不是的话则跳过
+				if not isExec:
+					print u"%s %s" % (filePath, cfg.STRINGS_RUNNING_MSG2)
+					continue
+
+				#读取文件部分
+				fileContent = None
+				fileObject = open(filePath)
+				try:
+					fileContent = fileObject.read()
+				finally:
+					fileObject.close()
+				#print fileContent
+				
+				#转换编码部分
+				if self.encodeUTF8Radio.GetValue():
+					#GBK转换为UTF-8
+					try:
+						fileContent = fileContent.decode("GBK").encode("UTF-8")
+					except UnicodeDecodeError, e:
+						msg = "%s %s" % (filePath, cfg.STRINGS_DIALOG_MSG5)
+						alert = wx.MessageDialog(None, msg, cfg.STRINGS_DIALOG_TITLE, style = wx.OK, pos = (100, 100))
+						alert.ShowModal()
+						continue
+
+				elif self.encodeGBKRadio.GetValue():
+					#UTF-8转换为GBK
+					try:
+						fileContent = fileContent.decode("UTF-8").encode("GBK")
+					except UnicodeDecodeError, e:
+						msg = "%s %s" % (filePath, cfg.STRINGS_DIALOG_MSG5)
+						alert = wx.MessageDialog(None, msg, cfg.STRINGS_DIALOG_TITLE, style = wx.OK, pos = (100, 100))
+						alert.ShowModal()
+						continue
+				
+				#写入文件部分
+				fileObject = open(filePath, "w")
+				fileObject.write(fileContent)
+				fileObject.close()
+
+				print u"%s %s" % (filePath, cfg.STRINGS_RUNNING_MSG1)
 	
 	def EnableUI(self, isEnable):
+		"""
+		是否启用窗口上面的控件
+		"""
+
 		self.filesFilterTextCtrl.Enable(isEnable)
 		self.dirTextCtrl.Enable(isEnable)
+		self.encodeUTF8Radio.Enable(isEnable)
 		self.encodeGBKRadio.Enable(isEnable)
 		self.selectDirButton.Enable(isEnable)
 		self.runButton.Enable(isEnable)
 
 	def FinishChange(self):
+		"""
+		完成操作流程后执行
+		"""
+
 		alert = wx.MessageDialog(None, cfg.STRINGS_DIALOG_MSG4, cfg.STRINGS_DIALOG_TITLE, style = wx.OK, pos = (100, 100))
 		alert.ShowModal()
 		self.EnableUI(True)
